ec6c6913dfd02f9a3d7492c663a3719386b10a18
anapple96 <62083669+anapple96@users.noreply.github.com>
2020-08-13 19:45:08 +0700

    Stuffs

---
+++ src/scripts/partySlots/PartySlotController.ts
@@ -0,0 +1,6 @@
+class PartySlotController {
+    public static getPokemonImage(pokemon: string) {
+        let pType = PokemonHelper.getPokemonByName(pokemon).id;
+        return `assets/images/pokemon/${pType}.png`;
+    }
+}
