9b096869d7d25ccbabebb0b0783d063b0d6ce5c1
anapple96 <62083669+anapple96@users.noreply.github.com>
2020-08-13 21:26:48 +0700

    stuffs

--- src/scripts/partySlots/PartySlotController.ts
+++ src/scripts/partySlots/PartySlotController.ts
@@ -3,4 +3,9 @@ class PartySlotController {
         let pType = PokemonHelper.getPokemonByName(pokemon).id;
         return `assets/images/pokemon/${pType}.png`;
     }
+    public static openPartySlotModal() {
+        if (App.game.breeding.canAccess()) {
+            $('#partySlotModal').modal('show');
+        }
+    }
 }
