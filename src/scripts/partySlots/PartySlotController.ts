f9c4b0019770391c4b265fe2a19694f07f2336c4
anapple96 <62083669+anapple96@users.noreply.github.com>
2020-08-14 22:33:39 +0700

    things

--- src/scripts/partySlots/PartySlotController.ts
+++ src/scripts/partySlots/PartySlotController.ts
@@ -8,4 +8,11 @@ class PartySlotController {
             $('partyLayout').modal('show');
         }
     }
+    public static getPokemonMultiplier(pokemon: string) {
+        let pAttack = PokemonHelper.getPokemonByName(pokemon).attack;
+        if (pAttack <= 150) {
+            return;
+        }
+        return `x${(100 + Math.round(pAttack / 100)) / 100}`;
+    }
 }
